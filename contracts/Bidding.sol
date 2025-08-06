2c3b79d346ebe7a0b78aeac108be2a7f5b1c1cfd
ManagementBC <hawashindiana@gmail.com>
2025-08-06 22:20:44 +0400

    Add smart contracts

---
+++ contracts/Bidding.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Registration.sol";
+
+
+contract BiddingContract is Ownable(msg.sender) {
+    Registration registration;
+
+
+    // Struct to represent a bid
+    struct Bid {
+        address CAB;
+        uint256 amount;
+        bool selected;
+    }
+
+    // Struct to represent an auction
+    struct Auction {
+        uint256 equipmentId;
+        address manufacturer;
+        bool active;
+        uint256 bestBidId;
+        uint256 bidCounter;
+        mapping(uint256 => Bid) bids;
+        uint256 bestBidAmount;
+    }
+
+    uint256 public auctionCounter;
+    mapping(uint256 => Auction) public auctions;
+
+    // Events
+    event NewAuction(uint256 auctionId, uint256 equipmentId, address indexed manufacturer);
+    event NewBid(uint256 auctionId, uint256 bidId, address indexed CAB, uint256 amount);
+    event BestBidSelected(uint256 auctionId, uint256 bidId, address indexed CAB, uint256 amount);
+
+    // Modifiers
+    modifier onlyRegisteredManufacturers {
+        require(registration.registeredManufacturers(msg.sender), "Only registered manufacturers can run this function");
+        _;
+    }
+
+    modifier onlyRegisteredCABs {
+        require(registration.registeredCABs(msg.sender), "Only registered CABs can run this function");
+        (, , , , bool accredited) = registration.getCABDetails(msg.sender);
+        require(accredited, "CAB is not accredited");
+        _;
+    }
+
+    // Constructor
+    constructor(address registrationAddress) {
+        registration = Registration(registrationAddress);
+
+    }
+
+    // Function to create a new auction
+    function createAuction(uint256 _equipmentId) public onlyRegisteredManufacturers {
+        (, address manufacturer, , ) = registration.getEquipmentDetails(_equipmentId);
+        require(manufacturer == msg.sender, "You are not the manufacturer of this equipment");
+
+        auctionCounter++;
+        Auction storage newAuction = auctions[auctionCounter];
+        newAuction.equipmentId = _equipmentId;
+        newAuction.manufacturer = msg.sender;
+        newAuction.active = true;
+        newAuction.bestBidAmount = type(uint256).max;
+
+        emit NewAuction(auctionCounter, _equipmentId, msg.sender);
+    }
+
+    // Function to submit a bid
+    function submitBid(uint256 _auctionId, uint256 _amount) public onlyRegisteredCABs {
+        Auction storage auction = auctions[_auctionId];
+        require(auction.active, "Auction is not active");
+        require(_amount > 0, "Enter a valid amount");
+
+        auction.bidCounter++;
+        auction.bids[auction.bidCounter] = Bid(msg.sender, _amount, false);
+
+        if (_amount < auction.bestBidAmount) {
+            auction.bestBidAmount = _amount;
+            auction.bestBidId = auction.bidCounter;
+        }
+
+        emit NewBid(_auctionId, auction.bidCounter, msg.sender, _amount);
+    }
+
+    // Function to select the best bid (Time window can be included but it is outside the scope of our paper)
+    function selectBestBid(uint256 _auctionId) public payable {
+        Auction storage auction = auctions[_auctionId];
+        require(auction.active, "Auction is not active");
+        require(auction.manufacturer == msg.sender, "Only the manufacturer can select the best bid");
+
+        uint256 selectedBidId = auction.bestBidId;
+        require(selectedBidId != 0, "No valid bids found");
+
+        Bid storage bestBid = auction.bids[selectedBidId];
+        require(msg.value == bestBid.amount, "Amount sent does not match the best bid amount");
+
+
+        auction.bids[selectedBidId].selected = true;
+        auction.active = false;
+
+        // Transfer the bid amount to the CAB
+        payable(bestBid.CAB).transfer(bestBid.amount);
+
+        emit BestBidSelected(_auctionId, selectedBidId, auction.bids[selectedBidId].CAB, auction.bids[selectedBidId].amount);
+    }
+
+    // Function to get bid details
+    function getBidDetails(uint256 _auctionId, uint256 _bidId) public view returns (address, uint256, bool) {
+        Auction storage auction = auctions[_auctionId];
+        Bid memory bid = auction.bids[_bidId];
+        return (bid.CAB, bid.amount, bid.selected);
+    }
+
+    // Function to get auction details
+    function getAuctionDetails(uint256 _auctionId) public view returns (uint256, address, bool, uint256) {
+        Auction storage auction = auctions[_auctionId];
+        return (auction.equipmentId, auction.manufacturer, auction.active, auction.bestBidId);
+    }
+
+    // Function to get the winning CAB for an equipment
+    function getWinningCAB(uint256 _equipmentId) public view returns (address) {
+        Auction storage auction = auctions[_equipmentId];
+        require(auction.bestBidId != 0, "No winning CAB found");
+        return auction.bids[auction.bestBidId].CAB;
+    }
+}
---
+++ contracts/EquipmentAccreditation.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Bidding.sol";
+import "./CommonsLibrary.sol";
+
+contract EquipmentAccreditation is Ownable(msg.sender) {
+    using CommonsLibrary for CommonsLibrary.Status;
+
+    BiddingContract bidding;
+
+    struct TestResult {
+        uint256 equipmentId;
+        address cab;
+        string ipfsHash;
+        CommonsLibrary.Status accreditationStatus;
+        bool exists;
+        bool isRevoked;
+        string updatedIpfsHash;
+    }
+
+    mapping(uint256 => TestResult) public testResults;
+
+    event TestResultsSubmitted(uint256 equipmentId, address cab, string ipfsHash);
+    event AccreditationDecisionMade(uint256 equipmentId, CommonsLibrary.Status decision);
+    event AccreditationRevoked(uint256 equipmentId, address revokedBy, uint256 timestamp);
+    event AccreditationUpdated(uint256 equipmentId, string newIpfsHash, uint256 timestamp);
+    event AccreditationDecisionUpdated(uint256 equipmentId, CommonsLibrary.Status decision, uint256 timestamp);
+
+    constructor(address biddingAddress) {
+        bidding = BiddingContract(biddingAddress);
+    }
+
+    function submitTestResults(uint256 _equipmentId, string memory _ipfsHash) public {
+        require(bytes(_ipfsHash).length == 46, "Invalid IPFS hash length");
+        require(!testResults[_equipmentId].exists, "Test results already submitted");
+
+        address winningCAB = bidding.getWinningCAB(_equipmentId);
+        require(msg.sender == winningCAB, "Only the winning CAB can submit test results");
+
+        testResults[_equipmentId] = TestResult({
+            equipmentId: _equipmentId,
+            cab: msg.sender,
+            ipfsHash: _ipfsHash,
+            accreditationStatus: CommonsLibrary.Status.Pending,
+            exists: true,
+            isRevoked: false,
+            updatedIpfsHash: ""
+        });
+
+        emit TestResultsSubmitted(_equipmentId, msg.sender, _ipfsHash);
+    }
+
+    function makeAccreditationDecision(uint256 _equipmentId, CommonsLibrary.Status decision) public onlyOwner {
+        TestResult storage result = testResults[_equipmentId];
+        require(result.exists, "Test results do not exist");
+        require(decision == CommonsLibrary.Status.Approved || decision == CommonsLibrary.Status.Denied, "Invalid decision value");
+
+        result.accreditationStatus = decision;
+
+        emit AccreditationDecisionMade(_equipmentId, decision);
+    }
+
+    function revokeAccreditation(uint256 _equipmentId) external onlyOwner {
+        TestResult storage result = testResults[_equipmentId];
+        require(result.exists, "Test results do not exist");
+        require(!result.isRevoked, "Already revoked");
+
+        result.isRevoked = true;
+
+        emit AccreditationRevoked(_equipmentId, msg.sender, block.timestamp);
+    }
+
+    function updateAccreditation(uint256 _equipmentId, string memory newIpfsHash) external onlyOwner {
+        TestResult storage result = testResults[_equipmentId];
+        require(result.exists, "Test results do not exist");
+        require(!result.isRevoked, "Accreditation revoked");
+
+        result.updatedIpfsHash = newIpfsHash;
+
+        emit AccreditationUpdated(_equipmentId, newIpfsHash, block.timestamp);
+    }
+
+    function confirmUpdatedAccreditation(uint256 _equipmentId, CommonsLibrary.Status decision) external onlyOwner {
+        TestResult storage result = testResults[_equipmentId];
+        require(result.exists, "Test results do not exist");
+        require(!result.isRevoked, "Accreditation revoked");
+
+        result.accreditationStatus = decision;
+
+        emit AccreditationDecisionUpdated(_equipmentId, decision, block.timestamp);
+    }
+
+    function getTestResultDetails(uint256 _equipmentId) public view returns (uint256, address, string memory, CommonsLibrary.Status) {
+        TestResult storage result = testResults[_equipmentId];
+        require(result.exists, "Test results do not exist");
+
+        return (result.equipmentId, result.cab, result.ipfsHash, result.accreditationStatus);
+    }
+}
